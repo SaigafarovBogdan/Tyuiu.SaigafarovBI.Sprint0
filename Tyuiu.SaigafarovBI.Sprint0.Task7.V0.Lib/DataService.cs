104210b272642cecb560b4f4f177ee804a7c4dbe
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-11 12:40:50 +0500

    Task7

---
+++ Tyuiu.SaigafarovBI.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.SaigafarovBI.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int Length = 5;
+            int[] resultArray = new int[Length];
+            for (int i = 0;i<Length;++i)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
