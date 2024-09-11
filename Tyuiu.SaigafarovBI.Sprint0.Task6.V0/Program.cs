c3eae939d454aae19ac96c349986d0407c565481
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-11 12:04:15 +0500

    Task6

---
+++ Tyuiu.SaigafarovBI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SaigafarovBI.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.SaigafarovBI.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] {1,2,3,4,5};
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элментов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
