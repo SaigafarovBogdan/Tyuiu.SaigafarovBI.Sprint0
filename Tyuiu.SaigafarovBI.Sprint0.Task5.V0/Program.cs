8b095bf3500abe9450c9b7dce4ff2efd011e9a4f
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-09-11 11:27:44 +0500

    Task5

---
+++ Tyuiu.SaigafarovBI.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.SaigafarovBI.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.SaigafarovBI.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataSevice.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataSevice.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataSevice.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataSevice.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
